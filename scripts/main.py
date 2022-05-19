464d19bdac02920b0766139547946cafb222ab1d
Igor Brinker <igorbrinker@gmail.com>
2022-05-19 15:35:28 -0300

    Converting output to json

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import json
 from datetime import datetime
 
 def raw_data():
@@ -8,6 +9,7 @@ def raw_data():
     raw_email = input('Insert yout e-mail address: ')
     # print(raw_email)
     domain = raw_email.split('@')[1]
+    domain = re.sub(r'<(.*?)>', '', domain)
     print(domain)
 
     subdomain = raw_email.split('@')[0]
@@ -26,7 +28,7 @@ def raw_data():
     print(cleansed_email)
 
     time = datetime.timestamp(now)
-    timestamp = datetime.fromtimestamp(time)
+    timestamp = datetime.fromtimestamp(time).isoformat()
     print(timestamp)
 
     if len(domain.split('.')[1]) >= 2:
@@ -34,6 +36,21 @@ def raw_data():
     else: domain_valid_indicator = False
 
     print(domain_valid_indicator)
+
+    # if 
+
+    result = [{
+        'raw_email': raw_email,
+        'domain': domain,
+        'doman_valid_indicator': domain_valid_indicator,
+        'validation_timestamp': timestamp,
+        'username_valid_indicator': 'true',
+        'cleansed_email': cleansed_email
+    }]
+
+    with open('../data/output.json', 'w') as f:
+        json.dump(result, f)
+
 
 
 raw_data()
